a85bb9f4b13d26823a17ed14fbf204429e492c61
GCHQ 11 <gchq.developer.11@gmail.com>
2019-04-27 22:15:17 +0100

    organising

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-myName = "Joe";
-
-console.log(`My Name is ${myName}`);
-console.log("How do you spell that?");
-for (const c of myName) {
-  console.log(c);
-}
-
-console.log(`First character is ${myName[0]}`);
-console.log(`Last character is ${myName[myName.length - 1]}`);
-console.log(`Length of name is ${myName.length}`);
+console.log('This is just the entry point');
---
+++ practicalProgramming/positionSub.js
@@ -0,0 +1,6 @@
+phrase = "How now brown cow"
+cowIndex = phrase.indexOf("cow")
+console.log(`The substring 'cow' in ${phrase} can be found at ${cowIndex}`)
+
+crowIndex = phrase.indexOf("crow")
+console.log(`Crow index in phrase is ${crowIndex}`)
