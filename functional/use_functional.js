4289afe9e67a4b8d737b9c8b38a5d0d2aecbc895
Joe Sharp <joe.martin.sharp@gmail.com>
2019-08-08 22:14:58 +0100

    chaining

--- functional/use_functional.js
+++ functional/use_functional.js
@@ -1,11 +1,19 @@
 my_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(`My Numbers ${my_numbers}`);
 
-my_squares = my_numbers.map(x => x * x);
+// Define some functions
+let square = x => x * x;
+let is_even = x => x % 2 == 0;
+let sum = (x, y) => x + y;
+
+my_squares = my_numbers.map(square);
 console.log(`My Squares ${my_squares}`);
 
-my_even_numbers = my_numbers.filter(x => x % 2 == 0);
+my_even_numbers = my_numbers.filter(is_even);
 console.log(`My Even Numbers ${my_even_numbers}`);
 
-sum_all = my_numbers.reduce((acc, curr) => acc + curr, 0);
+sum_all = my_numbers.reduce(sum, 0);
 console.log(`My Sum ${sum_all}`);
+
+even_squares = my_numbers.map(square).filter(is_even);
+console.log(`Even Squares ${even_squares}`);
