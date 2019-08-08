a61ad7c073fa58d49368cca497dddc9fd97a8a19
Joe Sharp <joe.martin.sharp@gmail.com>
2019-08-08 22:16:31 +0100

    commenting

--- functional/use_functional.js
+++ functional/use_functional.js
@@ -1,3 +1,4 @@
+// Give ourself a list of numbers
 my_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(`My Numbers ${my_numbers}`);
 
@@ -6,14 +7,18 @@ let square = x => x * x;
 let is_even = x => x % 2 == 0;
 let sum = (x, y) => x + y;
 
+// Demo Mapping to squares
 my_squares = my_numbers.map(square);
 console.log(`My Squares ${my_squares}`);
 
+// Demo filtering in even numbers
 my_even_numbers = my_numbers.filter(is_even);
 console.log(`My Even Numbers ${my_even_numbers}`);
 
+// Demo reducing as a sum
 sum_all = my_numbers.reduce(sum, 0);
 console.log(`My Sum ${sum_all}`);
 
+// Demo chaining mapping into filtering
 even_squares = my_numbers.map(square).filter(is_even);
 console.log(`Even Squares ${even_squares}`);
